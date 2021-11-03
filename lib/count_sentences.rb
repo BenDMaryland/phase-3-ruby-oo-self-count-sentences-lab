6a17c9f909dacbcb455c56f19248160f033f82b7
ben.d.maryland@gmail.com <bend@RoseFan.localdomain>
2021-11-03 18:43:03 -0400

    commit tree 0332bb2d363231dcc2cb35a6b7c7ba6666cd28e0

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,32 @@
 require 'pry'
 
 class String
+  # attr_accessor :sentence?, :question?, :exclamation?
 
   def sentence?
-    
+    self.slice(-1)=="."
   end
 
   def question?
-
+    
+    self.slice(-1)=="?"
   end
 
   def exclamation?
-
+    self.slice(-1)=="!"
   end
 
   def count_sentences
+ 
+    total_count=0
+    array=self.split(//)
+   
+   period_count= array.filter {|letter| letter==="."  }
+   question_count = array.filter {|letter| letter==="?"  }
+  exla_count= array.filter {|letter| letter==="!"  }
 
+  total_count= period_count+exla_count+question_count
+  binding.pry
+    total_count.length()
   end
 end
